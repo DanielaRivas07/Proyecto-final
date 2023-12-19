04271bde79ae29d8fbe6317a557b1b6ece561f82
Daniela Rivas <garuana.09@gmail.com>
2023-12-18 22:17:36 -0300

    primero

---
+++ proyecto2/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Header from "./componentes/Header/header";
+import About from "./componentes/about/about";
+
+
+
+function App() {
+
+    
+  return (
+    <>
+    
+    <Header />
+    <About />
+    </>
+  );
+
+  }
+export default App;
---
+++ proyecto2/src/componentes/about/about.jsx
@@ -0,0 +1,16 @@
+import "./about.css";
+
+
+function About() {
+  
+
+  return (
+    <>
+    
+      
+    </>
+  );
+}
+
+export default About;
+
